a9dfa165e2b53bf06ee2907e0bf7fb2e7fe3f22a
LuisGalicia <luchitogalicia24@gmail.com>
2018-06-28 10:03:26 -0400

    Initial

---
+++ ex1_LinReg/ex1_main.py
@@ -0,0 +1,12 @@
+from warmUp import identity_warmup
+
+def main():
+    print('Running Warm Up Exercise')
+    idMatrix = identity_warmup()
+    
+    print('5x5 Identity Matrix:\n', idMatrix)
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ ex1_LinReg/warmUp.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def identity_warmup():
+    # Write here the (5x5) identity matrix with numpy
+    idMatrix = np.eye(5)
+    
+    return idMatrix
+
+
