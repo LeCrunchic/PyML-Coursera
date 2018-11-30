611aab925d6029a457f7ad40a6587b3d2389908e
LeCrunchic <luisgalicia8@hotmail.com>
2018-11-30 14:51:12 -0400

    fixed 1st plot ex3

--- ex3_oneVsAll/ex3.py
+++ ex3_oneVsAll/ex3.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
-from utils import display_data
+from utils import display_data, compute_cost, compute_gradient
 # Defining neccesary parameters
 input_layer_size = 400 # 20x20 images
 num_labels = 10
@@ -21,3 +21,29 @@ idx_array = np.arange(m)
 rand_indxs = np.random.choice(idx_array, size=100, replace=False)
 
 display_data(X[rand_indxs, :])
+
+# ========== Test Logistic Regression ============ #
+
+theta_t = np.array([-2, -1, 1, 2])
+X_t = np.concatenate([ np.ones((5,1)) , np.arange(1, 16).reshape(5, 3, order='F') / 10], axis=1)
+y_t = np.array([[1], [0], [1], [0], [1]]) >= 0.5
+lambda_t = 3
+
+cost = compute_cost(theta_t, X_t, y_t, regularized=True, lambda_=lambda_t)
+grad = compute_gradient(theta_t, X_t, y_t, regularized=True,lambda_=lambda_t)
+
+print(cost, grad)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ex3_oneVsAll/utils.py
+++ ex3_oneVsAll/utils.py
@@ -4,6 +4,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import ix_
 
+
+def compute_cost(theta, x, y, regularized=False, lambda_=None):
+    m = y.shape[0]
+    J = -(1./m) * np.sum(y * np.log(sigmoid(x.dot(theta))) + (1. - y) * np.log(1. - sigmoid(x.dot(theta))) )
+
+    if regularized:
+        # notice that we do not regularize the bias term (we start from the second element in theta)
+        reg_term = lambda_ / (2*m) * np.sum(np.square(theta[1:]))
+        J += reg_term
+
+    return J
+
+def compute_gradient(theta, x, y, regularized=False, lambda_=None):
+    m = y.shape[0]
+    grad = (1./m) * ((sigmoid(x.dot(theta)) - y).T.dot(x)).T
+
+    import pdb; pdb.set_trace()
+    if regularized:
+        reg_term = (lambda_ / m) * theta[1:]
+        grad = np.hstack((grad[0], grad[1:] + reg_term))
+    return grad
+
 def display_data(x, example_width=None):
     if not example_width:
         example_width = floor(sqrt(x.shape[1]))
@@ -19,26 +41,27 @@ def display_data(x, example_width=None):
     display_array = -np.ones((pad + display_rows * (example_height + pad),
                              pad + display_cols * (example_width + pad)))
 
-    import pdb; pdb.set_trace()
     current_ex = 0
-    for j in range(display_rows - 1):
-        for i in range(display_cols - 1):
+    for j in range(display_rows):
+        for i in range(display_cols):
             if current_ex > m:
                 break
 
-            max_val = np.max(np.abs(x[current_ex, :]))
+            #max_val = np.max(np.abs(x[current_ex, :]))
 
             m_idxer = pad + (j-1) * (example_height + pad) + np.arange(0, example_height)
             n_idxer = pad + (i-1) * (example_width + pad) + np.arange(0, example_width)
 
             display_array[ix_(m_idxer, n_idxer)] = \
-                                  x[current_ex, :].reshape(example_height, example_width, order='F') / max_val
+                                  x[current_ex, :].reshape(example_height, example_width, order='F') #/ max_val
 
             current_ex += 1
 
         if current_ex > m:
             break
 
-    pdb.set_trace()
-    plt.imshow(display_array)
+    plt.imshow(display_array, cmap='gray_r')
     plt.show()
+
+def sigmoid(x):
+    return 1. / (1. + np.exp(-x))
